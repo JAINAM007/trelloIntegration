32e7c4b307c25d89e2df8577d425e8814b6c8fdd
jainam <jainamshah0079@gmail.com>
2023-06-06 18:48:32 -0400

    trello-OAuth-base-code

--- speakaiTrelloIntegration/server/server.js
+++ speakaiTrelloIntegration/server/server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const querystring = require('querystring');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const port = 3000; // Change this port number if needed
 
@@ -15,6 +18,26 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+// Redirect the user to Trello's authorization page
+app.get('/auth/trello', (req, res) => {
+    const params = {
+      response_type: 'token',
+      key: process.env.TRELLO_API_KEY,
+      return_url: process.env.CALLBACK_URL,
+    };
+  
+    const queryString = querystring.stringify(params);
+    const authUrl = `https://trello.com/1/authorize?${queryString}`;
+  
+    res.redirect(authUrl);
+  });
+
+//After oauth redirection 
+app.get('/auth/trello/callback', (req, res) => {
+    const { token } = req.query;
+    console.log(token);
+  });
 
 // Start the server and console it to check the server is running
 app.listen(port, () => {
